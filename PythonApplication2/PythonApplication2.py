6646cd5314bfad4422f30aa7381c6aa10ab045b2
psonthalia <pratik.sonthalia@biourja.com>
2018-07-18 17:55:30 +0530

    Adding test notebook

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -2,6 +2,6 @@ import pyodbc
 print('Hello World')
 print('Hello World')
 print('Hello Hello')
-print('Hello Hello World')'
+print('Hello Hello World')
 print('Hello Hello World 2')
 print('Hello Hello World 3')
